d77d0aa62f9ae5be602f7c8d6ca32cd2c91cb3ef
Raj Kumar <74557540+raaj157@users.noreply.github.com>
2024-03-29 16:26:20 +0530

    Add files via upload

---
+++ TwitterClone/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    extend: {
+      colors: {
+        'slate-950': '#your-color-hex-value',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
